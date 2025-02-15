e4533e500f89ece2a1c5ee39072fd4ab0345ed3c
AsmitNepali <33512152+AsmitNepali@users.noreply.github.com>
2025-02-15 23:21:59 +0545

    FIX: Url issue.

--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -6,11 +6,12 @@ final class Helper
 {
     public static function getResolvedUrl(string|int $key): string
     {
-        if (blank(config('filament-mention.mentionable.model.url'))) {
+        if (blank(config('filament-mention.mentionable.url'))) {
             return '#';
         }
 
-        return url(str_replace('{id}', $key, config('filament-mention.mentionable.model.url')));
+        return url(str_replace('{id}', $key, config('filament-mention.mentionable.url')));
+
 
     }
 }
