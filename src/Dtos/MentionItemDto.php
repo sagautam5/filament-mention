05a93708b949855acbba893cc51fcf7cd9b92df8
AsmitNepali <mail2asmitnepali@gmail.com>
2025-04-09 22:27:20 +0545

    ADD: Introduce Dto pattern

---
+++ src/Dtos/MentionItemDto.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Asmit\FilamentMention\Dtos;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+readonly class MentionItemDto implements Arrayable
+{
+    public function __construct(
+        public int $id,
+        public string $userName,
+        public string $displayName,
+        public string $avatar,
+        public string $url,
+    ) {}
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'user_name' => $this->userName,
+            'display_name' => $this->displayName,
+            'avatar' => $this->avatar,
+            'url' => $this->url,
+        ];
+    }
+}
